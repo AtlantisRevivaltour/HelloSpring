bfa84f523e94325c843e2fb45d5a8066da9ce7ae
AtlantisRevivaltour <mail.yangchun@163.com>
2016-06-17 15:08:39 +0800

    Initial commit

---
+++ src/com/atlantis/MainApp.java
@@ -0,0 +1,16 @@
+package com.atlantis;
+
+import com.atlantis.code.HelloSpring;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by Syusuke on 2016/6/17 0017.
+ */
+public class MainApp {
+    public static void main(String[] args){
+        ApplicationContext content =new ClassPathXmlApplicationContext("SpringBeans.xlm");
+        HelloSpring obj = (HelloSpring) content.getBean("helloSpring");
+        obj.getMessage();
+    }
+}
---
+++ src/com/atlantis/code/HelloSpring.java
@@ -0,0 +1,16 @@
+package com.atlantis.code;
+
+/**
+ * Created by Syusuke on 2016/6/17 0017.
+ */
+public class HelloSpring {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
