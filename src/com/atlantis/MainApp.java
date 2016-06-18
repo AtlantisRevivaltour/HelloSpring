a04cc9de5f40fa47c6f202e7f70ad015305e77f3
AtlantisRevivaltour <mail.yangchun@163.com>
2016-06-18 13:09:11 +0800

    comit

--- src/com/atlantis/MainApp.java
+++ src/com/atlantis/MainApp.java
@@ -5,11 +5,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Created by Syusuke on 2016/6/17 0017.
+ * Created by Syusuke on 2016/6/17 0017............
  */
 public class MainApp {
     public static void main(String[] args){
-        ApplicationContext content =new ClassPathXmlApplicationContext("SpringBeans.xlm");
+        ApplicationContext content =new ClassPathXmlApplicationContext("SpringBeans.xml");
         HelloSpring obj = (HelloSpring) content.getBean("helloSpring");
         obj.getMessage();
     }
